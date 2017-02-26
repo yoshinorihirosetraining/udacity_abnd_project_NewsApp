c7b21176156fbd1e4324cadf0f3b29a1a73860ba
Yoshinori Hirose <44sheetsofseaweed@gmail.com>
2017-02-26 11:21:07 +0900

    Add SearchView in Toolbar of MainActivity

--- app/src/main/java/yoshinorihirosetraining/gmail/com/newsmeijin/MainActivity.java
+++ app/src/main/java/yoshinorihirosetraining/gmail/com/newsmeijin/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        SearchView search = (SearchView) findViewById(R.id.search);
+        search.setLayoutParams(new Toolbar.LayoutParams(Gravity.RIGHT));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
